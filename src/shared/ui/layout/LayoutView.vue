f8aaada46e1b0b15a4d8d21556415bda2e0157a6
YaMamaLul <terentiy51@gmail.com>
2024-02-03 14:52:21 +0500

    started layout

--- src/shared/ui/layout/LayoutView.vue
+++ src/shared/ui/layout/LayoutView.vue
@@ -1,19 +1,20 @@
 <script setup>
-import styles from "./Layout.module.scss"
-
-
+import styles from './Layout.module.scss'
 </script>
 
 <template>
   <div :class="styles.layoutWrapper">
     <div :class="styles.layoutTop">
       123213
+
+      <div :class="styles.userInfo">
+        <span> Terentii </span>
+        <span> terentii@mail.ru </span>
+      </div>
     </div>
 
-    <div :class="styles.layoutLeft">
-      4345
-    </div>
+    <div :class="styles.layoutLeft">4345</div>
 
-    <slot/>
+    <slot />
   </div>
 </template>
